c89eeda4b2f0596aa071d942aa81d56e3fb7b269
SWAPNIL BAWANKAR <swapnilb6@gmail.com>
2022-08-02 15:23:54 +0530

    Add project files.

---
+++ MvcWeb.IdentityFrameworkAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcWeb.IdentityFrameworkAuthentication.Startup))]
+namespace MvcWeb.IdentityFrameworkAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
